fde0e0560df9b0bf3f5a96890595f014da8f71c4
laurapesonen <laura-pesonen@hotmail.com>
2022-06-27 13:50:22 +0300

    Ensimmäinen metodi

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+
+    
+    public class Class1
+
+        {
+        //public bool timeCheck(DateTime input);
+        DateTime d1 = new DateTime(2022, 06, 30);
+
+        if()
+
+        }
+
+    
+}
+
+
+//Metodi, joka tarkistaa onko annettu päivämäärä tulevaisuudessa ja palauttaa true mikäli päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä
+//Metodi, joka kertoo opiskelun aloitusajan ja arvioidun opiskeluajan perusteella onko opiskelu myöhässä vai ei. 
---
+++ classTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace classTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2022, 06, 30);
+            DateTime d2 = new DateTime(2022, 06, 27);
+
+            int result = DateTime.Compare(d1, d2);
+
+                Console.WriteLine(result);
+
+            bool resultb = false;
+            
+            if(result < 0)
+            {
+                resultb = true;
+                
+            }
+            Console.WriteLine(resultb);
+        }
+
+
+
+    }
+}
